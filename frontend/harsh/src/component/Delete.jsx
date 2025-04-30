c09505536895d2b5f3d875719e70adffb69ef3e5
HA <haagarwal67@gmail.com>
2025-04-30 10:03:57 +0530

    crud operations edit

--- frontend/harsh/src/component/Delete.jsx
+++ frontend/harsh/src/component/Delete.jsx
@@ -5,7 +5,7 @@ function Delete() {
     const handleDelete=async(e)=>{
         e.preventDefault();
         const id=e.target.id.value;
-        await axios.delete(`http://localhost:9000/users/${id}`)
+        await axios.delete(`https://new-connection-main-3.onrender.com/users/${id}`)
         alert("data delete")
     }
   return (
